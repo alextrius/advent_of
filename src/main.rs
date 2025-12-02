06e0b8be4a2277e29c8b1521f43e2b79dce8c7fd
GadOf <sda098123@gmail.com>
2025-12-03 00:14:53 +0300

    task1 complete, making task 2

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,41 @@ fn main() {
     let mut s = String::new();
     match file.read_to_string(&mut s) {
         Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => print!("{} contains:\n{}", display, s),
+        Ok(_) => println!("opened"),
     }
 
     let lines : Vec<String> = s.lines().map(|line| line.to_string()).collect();
 
+    let mut pos: i32 = 50;
     for line in lines {
-        println!("{}",line);
+        let (direction,number_str) = line.split_at(1);
+        let mut number : i32 = number_str.parse().unwrap();
+
+        if (number > 99) {
+            res += number / 100;
+            number = number % 100;
+        }
+
+        if (direction == "R"){
+            pos += number;
+            if(pos > 99) {
+                res += 1;
+                pos -= 100;
+            }
+        } else {
+            pos -= number;
+            if (pos < 0){
+                res += 1;
+                pos += 100;
+            }
+        }
+
+        if(pos == 0){
+            res += 1;
+        }
     }
+
+
+
+    println!("{}",res);
 }
